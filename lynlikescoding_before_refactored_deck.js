4ea757f84669e8dd2d0b7c616687edb790ae39f4
Carolyn Lam <caro_lyn_@hotmail.com>
2016-11-04 11:21:01 -0400

    add logic to disable buttons that are not in use

--- lynlikescoding_before_refactored_deck.js
+++ lynlikescoding_before_refactored_deck.js
@@ -1,3 +1,58 @@
+// var deck = [
+//   { point: 1, suit: 'hearts'},
+//   { point: 2, suit: 'hearts'},
+//   { point: 3, suit: 'hearts'},
+//   { point: 4, suit: 'hearts'},
+//   { point: 5, suit: 'hearts'},
+//   { point: 6, suit: 'hearts'},
+//   { point: 7, suit: 'hearts'},
+//   { point: 8, suit: 'hearts'},
+//   { point: 9, suit: 'hearts'},
+//   { point: 10, suit: 'hearts'},
+//   { point: 11, suit: 'hearts'},
+//   { point: 12, suit: 'hearts'},
+//   { point: 13, suit: 'hearts'},
+//   { point: 1, suit: 'diamonds'},
+//   { point: 2, suit: 'diamonds'},
+//   { point: 3, suit: 'diamonds'},
+//   { point: 4, suit: 'diamonds'},
+//   { point: 5, suit: 'diamonds'},
+//   { point: 6, suit: 'diamonds'},
+//   { point: 7, suit: 'diamonds'},
+//   { point: 8, suit: 'diamonds'},
+//   { point: 9, suit: 'diamonds'},
+//   { point: 10, suit: 'diamonds'},
+//   { point: 11, suit: 'diamonds'},
+//   { point: 12, suit: 'diamonds'},
+//   { point: 13, suit: 'diamonds'},
+//   { point: 1, suit: 'clubs'},
+//   { point: 2, suit: 'clubs'},
+//   { point: 3, suit: 'clubs'},
+//   { point: 4, suit: 'clubs'},
+//   { point: 5, suit: 'clubs'},
+//   { point: 6, suit: 'clubs'},
+//   { point: 7, suit: 'clubs'},
+//   { point: 8, suit: 'clubs'},
+//   { point: 9, suit: 'clubs'},
+//   { point: 10, suit: 'clubs'},
+//   { point: 11, suit: 'clubs'},
+//   { point: 12, suit: 'clubs'},
+//   { point: 13, suit: 'clubs'},
+//   { point: 1, suit: 'spades'},
+//   { point: 2, suit: 'spades'},
+//   { point: 3, suit: 'spades'},
+//   { point: 4, suit: 'spades'},
+//   { point: 5, suit: 'spades'},
+//   { point: 6, suit: 'spades'},
+//   { point: 7, suit: 'spades'},
+//   { point: 8, suit: 'spades'},
+//   { point: 9, suit: 'spades'},
+//   { point: 10, suit: 'spades'},
+//   { point: 11, suit: 'spades'},
+//   { point: 12, suit: 'spades'},
+//   { point: 13, suit: 'spades'}
+// ];
+
 function newDeck() {
   var deck = [];
   for (var i = 0; i < 4; i++) {
@@ -61,8 +116,6 @@ $(document).ready(function() {
     randomCardGenerator('player');
     randomCardGenerator('dealer');
 
-    $(this).prop('disabled', true);
-
   });
 
   function calculatePoints(cards) {
@@ -85,8 +138,6 @@ $(document).ready(function() {
     $('#player-points').text(currPlayerPoints);
     if (currPlayerPoints > 21) {
       $("#messages").text("BIG BUST!!");
-      $("#hit-button").prop('disabled', true);
-      $("#stand-button").prop('disabled', true);
     }
   });
 
@@ -117,23 +168,15 @@ $(document).ready(function() {
     if(currDealerPoints === currPlayerPoints) {
     $('#messages').text("Push!");
     }
-
-    $("#hit-button").prop('disabled', true);
-
   });
 
   $("#reset-button").click(function() {
-    $("#deal-button").prop('disabled', false);
-    $("#hit-button").prop('disabled', false);
-    $("#stand-button").prop('disabled', false);
-
     $("#player-hand").empty();
     $("#dealer-hand").empty();
     $('#player-points').text("");
     $('#dealer-points').text("");
     $('#messages').text("");
 
-    deck = newDeck();
     dealerPoints = [];
     playerPoints = [];
 
