96282ba7e927e443facf889db4e2275de8678efc
Carolyn Lam <caro_lyn_@hotmail.com>
2016-11-04 11:09:30 -0400

    add current logic

--- lynlikescoding.js
+++ lynlikescoding.js
@@ -1,60 +1,78 @@
-var deck = [
-  { point: 1, suit: 'hearts'},
-  { point: 2, suit: 'hearts'},
-  { point: 3, suit: 'hearts'},
-  { point: 4, suit: 'hearts'},
-  { point: 5, suit: 'hearts'},
-  { point: 6, suit: 'hearts'},
-  { point: 7, suit: 'hearts'},
-  { point: 8, suit: 'hearts'},
-  { point: 9, suit: 'hearts'},
-  { point: 10, suit: 'hearts'},
-  { point: 11, suit: 'hearts'},
-  { point: 12, suit: 'hearts'},
-  { point: 13, suit: 'hearts'},
-  { point: 1, suit: 'diamonds'},
-  { point: 2, suit: 'diamonds'},
-  { point: 3, suit: 'diamonds'},
-  { point: 4, suit: 'diamonds'},
-  { point: 5, suit: 'diamonds'},
-  { point: 6, suit: 'diamonds'},
-  { point: 7, suit: 'diamonds'},
-  { point: 8, suit: 'diamonds'},
-  { point: 9, suit: 'diamonds'},
-  { point: 10, suit: 'diamonds'},
-  { point: 11, suit: 'diamonds'},
-  { point: 12, suit: 'diamonds'},
-  { point: 13, suit: 'diamonds'},
-  { point: 1, suit: 'clubs'},
-  { point: 2, suit: 'clubs'},
-  { point: 3, suit: 'clubs'},
-  { point: 4, suit: 'clubs'},
-  { point: 5, suit: 'clubs'},
-  { point: 6, suit: 'clubs'},
-  { point: 7, suit: 'clubs'},
-  { point: 8, suit: 'clubs'},
-  { point: 9, suit: 'clubs'},
-  { point: 10, suit: 'clubs'},
-  { point: 11, suit: 'clubs'},
-  { point: 12, suit: 'clubs'},
-  { point: 13, suit: 'clubs'},
-  { point: 1, suit: 'spades'},
-  { point: 2, suit: 'spades'},
-  { point: 3, suit: 'spades'},
-  { point: 4, suit: 'spades'},
-  { point: 5, suit: 'spades'},
-  { point: 6, suit: 'spades'},
-  { point: 7, suit: 'spades'},
-  { point: 8, suit: 'spades'},
-  { point: 9, suit: 'spades'},
-  { point: 10, suit: 'spades'},
-  { point: 11, suit: 'spades'},
-  { point: 12, suit: 'spades'},
-  { point: 13, suit: 'spades'}
-];
+// var deck = [
+//   { point: 1, suit: 'hearts'},
+//   { point: 2, suit: 'hearts'},
+//   { point: 3, suit: 'hearts'},
+//   { point: 4, suit: 'hearts'},
+//   { point: 5, suit: 'hearts'},
+//   { point: 6, suit: 'hearts'},
+//   { point: 7, suit: 'hearts'},
+//   { point: 8, suit: 'hearts'},
+//   { point: 9, suit: 'hearts'},
+//   { point: 10, suit: 'hearts'},
+//   { point: 11, suit: 'hearts'},
+//   { point: 12, suit: 'hearts'},
+//   { point: 13, suit: 'hearts'},
+//   { point: 1, suit: 'diamonds'},
+//   { point: 2, suit: 'diamonds'},
+//   { point: 3, suit: 'diamonds'},
+//   { point: 4, suit: 'diamonds'},
+//   { point: 5, suit: 'diamonds'},
+//   { point: 6, suit: 'diamonds'},
+//   { point: 7, suit: 'diamonds'},
+//   { point: 8, suit: 'diamonds'},
+//   { point: 9, suit: 'diamonds'},
+//   { point: 10, suit: 'diamonds'},
+//   { point: 11, suit: 'diamonds'},
+//   { point: 12, suit: 'diamonds'},
+//   { point: 13, suit: 'diamonds'},
+//   { point: 1, suit: 'clubs'},
+//   { point: 2, suit: 'clubs'},
+//   { point: 3, suit: 'clubs'},
+//   { point: 4, suit: 'clubs'},
+//   { point: 5, suit: 'clubs'},
+//   { point: 6, suit: 'clubs'},
+//   { point: 7, suit: 'clubs'},
+//   { point: 8, suit: 'clubs'},
+//   { point: 9, suit: 'clubs'},
+//   { point: 10, suit: 'clubs'},
+//   { point: 11, suit: 'clubs'},
+//   { point: 12, suit: 'clubs'},
+//   { point: 13, suit: 'clubs'},
+//   { point: 1, suit: 'spades'},
+//   { point: 2, suit: 'spades'},
+//   { point: 3, suit: 'spades'},
+//   { point: 4, suit: 'spades'},
+//   { point: 5, suit: 'spades'},
+//   { point: 6, suit: 'spades'},
+//   { point: 7, suit: 'spades'},
+//   { point: 8, suit: 'spades'},
+//   { point: 9, suit: 'spades'},
+//   { point: 10, suit: 'spades'},
+//   { point: 11, suit: 'spades'},
+//   { point: 12, suit: 'spades'},
+//   { point: 13, suit: 'spades'}
+// ];
+
+function newDeck() {
+  var deck = [];
+  for (var i = 0; i < 4; i++) {
+    var suit = {
+      0: 'hearts',
+      1: 'diamonds',
+      2: 'clubs',
+      3: 'spades'
+    }
+    for (var k = 1; k <= 13; k++) {
+      deck.push(
+        { point: k, suit: suit[i] }
+      );
+    }
+  }
+  return(deck);
+}
 
 $(document).ready(function() {
-
+  var deck = newDeck();
   var dealerPoints = [];
   var playerPoints = [];
   var usedCards = [];
@@ -102,7 +120,7 @@ $(document).ready(function() {
 
   function calculatePoints(cards) {
   var sum2Cards = cards.map(function(card) {
-    if (card.point === 11 || card.point === 12 || card.point === 13) {
+    if (card.point > 10) {
       card.point = 10;
     }
     return card.point
